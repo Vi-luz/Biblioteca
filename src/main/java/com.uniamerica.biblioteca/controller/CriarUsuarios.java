177886c2e40595a6abd1505beb976eba15afdcc0
Vinnyda23 <vinnycontateme@gmail.com>
2026-08-12 14:51:55 -0300

    Record para CriarUsuarios

---
+++ src/main/java/com.uniamerica.biblioteca/controller/CriarUsuarios.java
@@ -0,0 +1,6 @@
+package com.uniamerica.biblioteca.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
+public record CriarUsuarios (String nome,String raAluno,String senha){
+}
